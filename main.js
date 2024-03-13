4d8b7ca8e47f7c98d2be01e25c324730601e27b2
Benjamin Clauß <90100610+bencl94@users.noreply.github.com>
2024-03-13 21:13:10 +0100

    Add ability to play one round
    - user input is formatted
    - seperate functions return message for user

--- main.js
+++ main.js
@@ -9,3 +9,49 @@ function getComputerChoice() {
         return 'Scissors'
     }
 }
+
+function playRound(playersChoice, computersChoice) {
+    function getLoserMsg() {
+        return `You Lose! ${computersChoice} beats ${playersChoice}`
+    }
+    function getWinnerMsg() { return 'You Win!'; }
+    function getTieMsg() { return "It's a Tie!"; }
+    function formatPlayersChoice(choice) {
+        return choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+    }
+
+    if (!playersChoice) {
+        return 'Choose to play';
+    }
+
+    playersChoice = formatPlayersChoice(playersChoice);
+
+    if (playersChoice == computersChoice) {
+        return getTieMsg();
+    }
+
+    if (playersChoice == 'Rock') {
+        if (computersChoice == 'Paper') {
+            return getLoserMsg();
+        } else {
+            return getWinnerMsg();
+        }
+    }
+
+    if (playersChoice == 'Paper') {
+        if (computersChoice == 'Rock') {
+            return getWinnerMsg();
+        } else {
+            getLoserMsg();
+        }
+    }
+
+    if (playersChoice == 'Scissors') {
+        if (computersChoice == 'Rock') {
+            return getLoserMsg();
+        }
+        else {
+            getWinnerMsg();
+        }
+    }
+}
